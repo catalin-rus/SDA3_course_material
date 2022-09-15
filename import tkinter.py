e15067ed8b13b0a7ad2d6e459013f18e5a9db71e
catalin-rus <c_rares07@yahoo.com>
2022-09-15 09:52:15 +0200

    Administration class

---
+++ import tkinter.py
@@ -0,0 +1,13 @@
+class Administration:
+
+    def __init__(self, working_hours, salary):
+        self.working_hours = working_hours
+        self.salary = salary
+        
+    def Schedule(self, Schedule = "Starts too early and ends at 14"):
+        print(Schedule)
+    
+    def Answer(self, Answer = "I may direct you to this office"):
+        print(Answer)
+    def EnrollStudent(self, EnrollStudent = "There was a problem in the system, please come back tomorrow"):
+        print(EnrollStudent)
